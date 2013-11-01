6cecfcf732b60b3a8dc7fb044e631f9b9ccb5fcf
Franz Roth <change_0703@froweb.ch>
2013-11-01 17:11:22 +0100

    Number of shown days can now be adjusted be constant AMOUNT_OF_DAYS

--- EwigaDuudel.php
+++ EwigaDuudel.php
@@ -7,13 +7,13 @@
   include 'CCookie.php';
   include 'CDoodleTable.php';
 
-  define("AMOUNT_OF_DAYS", "6");
+  define("AMOUNT_OF_DAYS", "7");
 
   $HTML = new HTMLOut();
   $Cookie = new Cookie();
 
-  $weekdays="So,Mo,Di,Mi,Do,Fr,Sa,So,Mo,Di,Mi,Do,Fr,Sa,So";
-  $weekdays=explode(",",$weekdays);
+  $cWeekDays = "Mo,Di,Mi,Do,Fr,Sa,So";
+  $cWeekDays = explode(",",$cWeekDays);
 
   function AdvanceADay() {
     $remaining = 0;
@@ -25,14 +25,14 @@
       $LineArray = explode(",",$line);
       //echo($line);
       $flag = "0";
-      for ($i = 2; $i <= 6; $i++) {
+      for ($i = 2; $i <= AMOUNT_OF_DAYS; $i++) {
         $LineArray[$i]= trim($LineArray[$i]);
         if (!empty($LineArray[$i])) { $flag = "1"; }
       }
       if ($flag == "1") { // only keep entry if there are active dates
         $remaining = $remaining + 1;
         fwrite($fs,$LineArray[0]);
-        for ($i = 2; $i <= 6; $i++) {
+        for ($i = 2; $i <= AMOUNT_OF_DAYS; $i++) {
           //echo ($i . " ");
           fwrite($fs,"," . trim($LineArray[$i]));
         }
@@ -65,7 +65,7 @@
   function CleanEntry($Entry) {
    $val = trim($Entry);
    $val = trim($val , ",");
-   $val = str_replace(",", ":",$val);
+   $val = str_replace(",", ":", $val);
    $val = "," . strip_tags($val);
    $val = substr($val, 0, 30);
    return $val;
@@ -85,7 +85,7 @@
   $fs = fopen("lastaccess.csv","w"); fwrite($fs,$todaysday); fclose($fs);
 
   if (@$_POST['formLogout'] == "Logout") {
-    setcookie ("KlimperName", "", time() - 3600);  // Clear Cookie
+    $Cookie->SetCookie(""); // Clear Cookie
     header("Location: Login.php");
   }
   if (!$Cookie->CheckCookieExists()) {
@@ -97,25 +97,28 @@
   }
 
   if(@$_POST['formSubmit'] == "Submit") {
-      $datestring = $Cookie->GetClimperName() . CleanEntry($_POST['ck1']) . CleanEntry($_POST['ck2']) . CleanEntry($_POST['ck3']) .
-                    CleanEntry($_POST['ck4']) . CleanEntry($_POST['ck5']) . CleanEntry($_POST['ck6']);
-      EditLine($datestring);
+      $DataString = $Cookie->GetClimperName();
+      for ($Day = 1; $Day <= AMOUNT_OF_DAYS; $Day++) {
+      	$DataString .= CleanEntry($_POST['ck'.$Day]);
+      }
+      EditLine($DataString);
   }
 
   $Table = new DoodleTable();
-
   $Form = new FormHandler("post", "Klimperform");
 
-  $weekday = @date(N);
-  $data = file("mydata.csv"); //or die('Could not read file!');
+  $ActualWeekDay = @date(N);
 
   // Generate Header with the days
   $Table->AddSingleCell("Klimper", CCell::COLOR_HEADER);
-  for ($i = $weekday; $i <= ($weekday + 5); $i++) {
-		$Table->AddSingleCell($weekdays[$i], CCell::COLOR_HEADER);
+  for ($i = $ActualWeekDay; $i <= ($ActualWeekDay + (AMOUNT_OF_DAYS -1)); $i++) {
+    $Index = $i % 7;
+    $Table->AddSingleCell($cWeekDays[$Index], CCell::COLOR_HEADER);
   }
   $Table->AddRow($Table->GetCellArray(), RowType::Header);
 
+
+  $data = file("mydata.csv"); //or die('Could not read file!');
   $ClimperAlreadyInDatabases = false;
   foreach ($data as $line) {
     $LineArray = explode(",",$line);
