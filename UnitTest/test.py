58d384cdf958b631434439006b5c693535d739ea
Franz Roth <change_0703@froweb.ch>
2014-03-23 18:07:56 +0100

    Add testDataHandler.php

--- UnitTest/test.py
+++ UnitTest/test.py
@@ -8,8 +8,11 @@ os.system("touch TestResult.log")
 os.system("phpunit testDoodleTable.php >> TestResult.log")
 os.system("phpunit testElement.php >> TestResult.log")
 os.system("phpunit testFormHandler.php >> TestResult.log")
+os.system("phpunit testDataHandler.php >> TestResult.log")
 
 os.system("cat TestResult.log")
 
+os.system("echo '======================='")
 os.system("grep FAILURES! TestResult.log")
+os.system("grep OK TestResult.log")
 
