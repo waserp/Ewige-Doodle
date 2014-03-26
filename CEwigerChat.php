29ddd1d0998e86d660f2a319a03b4bb961a884ee
Franz Roth <change_0703@froweb.ch>
2014-03-26 23:42:36 +0100

    Add basic functionality for chat

---
+++ CEwigerChat.php
@@ -0,0 +1,39 @@
+<?php
+
+define("FILE_CHAT_DATA", "chat.data");
+
+class CEwigerChat
+{
+	private $ChatData;
+
+	public function __construct()
+	{
+// 		$Text = "";
+// 		$fp = fopen(FILE_CHAT_DATA, "r");
+// 		while (!feof($fp)) {
+// 			$Text .= fgets($fp);
+// 		}
+// 		fclose($fp);
+//		$this->ChatData = $Text;
+	}
+
+	public function __destruct()
+	{
+
+	}
+
+	public function GetText()
+	{
+		$this->ChatData = file_get_contents(FILE_CHAT_DATA);
+		return $this->ChatData;
+	}
+
+	public function AddComment($NewText)
+	{
+    $ChatData = $NewText."\n\n";
+		$ChatData .= file_get_contents(FILE_CHAT_DATA);
+		file_put_contents(FILE_CHAT_DATA, $ChatData);
+	}
+}
+
+?>
---
+++ UnitTest/testEwigerChat.php
@@ -0,0 +1,48 @@
+<?php
+
+include '../CEwigerChat.php';
+
+class testEwigerChat  extends PHPUnit_Framework_TestCase
+{
+
+	public function __construct()
+	{
+		$this->BackupFiles();
+	}
+
+	public function __destruct()
+	{
+		$this->RestoreFiles();
+	}
+
+	public function testReadCurrent()
+	{
+		$Chat = new CEwigerChat();
+		$ActualText = $Chat->GetText();
+		$this->assertEquals("Test\nZeile2", $ActualText);
+	}
+
+	public function testAddComment()
+	{
+		$Chat = new CEwigerChat();
+		$NewText = "MyComment";
+		$Chat->AddComment($NewText);
+		unset($Chat);
+		$Chat = new CEwigerChat();
+		$ActualText = $Chat->GetText();
+		$this->assertEquals("MyComment\n\nTest\nZeile2", $ActualText);
+	}
+
+	private function BackupFiles()
+	{
+		copy(FILE_CHAT_DATA, FILE_CHAT_DATA.".bak");
+	}
+	private function RestoreFiles()
+	{
+		copy(FILE_CHAT_DATA.".bak", FILE_CHAT_DATA);
+	}
+
+
+}
+
+?>
